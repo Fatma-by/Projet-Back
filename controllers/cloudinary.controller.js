d73f80219d54332a2da19188c5ed9189e3c73e1b
Fatma-by <fatmabenyouness@gmail.com>
2024-02-18 11:52:00 +0100

    devoirDeuxiemeAnneeDone

--- controllers/cloudinary.controller.js
+++ controllers/cloudinary.controller.js
@@ -10,29 +10,25 @@ cloudinary.config({
   api_secret: process.env.CLOUD_SECRET,
 });
 
-
 const createRessource = async (req, res) => {
   try {
     console.log(req.headers);
     const { file } = req.body;
-   
+
     new Promise((resolve, reject) => {
       cloudinary.uploader.upload(file, async (error, result) => {
         if (result && result.secure_url) {
-;
-         
-            const newRessource = await Resource.create({
-              NomEnseignant : req.headers.name,
-              Niveau : req.headers.selectedniveau,
-              Matiere : req.headers.selectedmatiere,
-              Nature :req.headers.selectednature,
-              Document : {
-                url : result.secure_url,
-                public_id : result.public_id
-              },
-              Trimestre : req.headers.selectedtrimestre
-  
-            })
+          const newRessource = await Resource.create({
+            NomEnseignant: req.headers.name,
+            Niveau: req.headers.selectedniveau,
+            Matiere: req.headers.selectedmatiere,
+            Nature: req.headers.selectednature,
+            Document: {
+              url: result.secure_url,
+              public_id: result.public_id,
+            },
+            Trimestre: req.headers.selectedtrimestre,
+          });
           res.status(201).json({});
 
           return resolve(result.secure_url);
@@ -41,7 +37,6 @@ const createRessource = async (req, res) => {
         return reject({ message: error.message });
       });
     });
-
   } catch (err) {
     console.log(err.message);
     res.status(500).json({ message: "error" });
@@ -50,32 +45,50 @@ const createRessource = async (req, res) => {
 
 const getRessources = async (req, res) => {
   try {
-    const data = req.body
-    console.log(data)
-    let query = {}
+    const data = req.body;
+    console.log("here", data);
+    let query = {};
     switch (data.code) {
-      case "'s/2/d/1'":
+      case "s/2/d/1":
         query = {
           Nature: "Devoir",
           Matiere: "Science",
           Niveau: "Deuxième Année",
           Trimestre: "Première Trimèstre",
-        }
-        
+        };
+        break; 
+        case "s/2/d/2":
+            query = {
+              Nature: "Devoir",
+              Matiere: "Science",
+              Niveau: "Deuxième Année",
+              Trimestre: "Deuxième Trimèstre",
+           }
+           break; 
+           case "s/2/d/3":
+            query = {
+              Nature: "Devoir",
+              Matiere: "Science",
+              Niveau: "Deuxième Année",
+              Trimestre: "Troisième Trimèstre",
+           }
+
         break;
-    
+
       default:
         break;
     }
-    const Ressources= await Resource.find(query)
-    res.status(200).json({Ressources: Ressources});
+    const Ressources = await Resource.find(query);
+    res.status(200).json({ Ressources: Ressources });
   } catch (error) {
     console.error(error.message);
-    res.status(500).json({ message: 'Erreur lors de la récupération des devoirs' });
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la récupération des devoirs" });
   }
 };
 
 module.exports = {
   createRessource,
-  getRessources
-}
+  getRessources,
+};
